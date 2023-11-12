3649d1fb72f2c3b699ce0738703f40b91fc5173d
geisabitt <ageisislane@gmail.com>
2023-11-12 14:38:27 -0300

    style(global): Add global styles

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Montserrat, sans-serif; 
+    color: #252B42;
+    & a{
+        color: #252B42;
+        cursor: pointer;
+    }
+  }
+
+`;
+
+export default GlobalStyle;
